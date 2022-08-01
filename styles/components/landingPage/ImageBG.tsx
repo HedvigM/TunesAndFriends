e172a5d915ca6d73dd5c14f5fca83ae44d055d59
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-01 23:42:06 +0200

    Update ImageBG.tsx

--- styles/components/landingPage/ImageBG.tsx
+++ styles/components/landingPage/ImageBG.tsx
@@ -1,14 +1,25 @@
 import { Box, Typography } from '@mui/material';
-import { display } from '@mui/system';
 import React from 'react';
+import plant from '../../../assets/plant.jpeg';
+import Image from 'next/image';
 
 export const ImageBG = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: 'primary.dark',
-      }}
-    >
+    <Box>
+      {/*  <Image
+        src='/../../../assets/plant.jpeg'
+        height='100%'
+        width='100%'
+        fit='fill'
+        duration={3000}
+        easing='cubic-bezier(0.7, 0, 0.6, 1)'
+        showLoading={true}
+        errorIcon={true}
+        shift={null}
+        distance='100px'
+        shiftDuration={900}
+        bgColor='inherit'
+      /> */}
       <Typography
         variant={'h2'}
         mt={2}
