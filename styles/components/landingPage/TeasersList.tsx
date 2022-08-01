587c17a46eacf3e2cf1644c4b255ec9c95a1c1d0
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-01 23:44:55 +0200

    feature: different content if loged in

--- styles/components/landingPage/TeasersList.tsx
+++ styles/components/landingPage/TeasersList.tsx
@@ -11,79 +11,87 @@ import Box from '@mui/material/Box';
 import PianoIcon from '@mui/icons-material/Piano';
 import { Container } from '@mui/system';
 import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export const TeasersList = () => {
+  const { user } = useUser();
   return (
     <>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            margin: '10px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            backgroundColor: 'primary.dark',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-        >
-          <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
-          <Typography variant={'body1'} sx={{ padding: '10%' }}>
-            Gather your friends to a session
-          </Typography>
+      {user ? (
+        <Typography variant={'h1'} sx={{ padding: '10%' }}>
+          Welcome, {user.name} to the Tunes and friends community!
+        </Typography>
+      ) : (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              margin: '10px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
+            <Typography variant={'body1'} sx={{ padding: '10%' }}>
+              Gather your friends to a session
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              margin: '10px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            {' '}
+            <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
+            <Typography variant={'body1'} sx={{ padding: '10%' }}>
+              Mark the tunes you know and the ones you would like to learn.
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              margin: '10px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
+            <Typography variant={'body1'} sx={{ padding: '10%' }}>
+              Find out which tunes you and your friends have in common.
+            </Typography>
+          </Box>
         </Box>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            margin: '10px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            backgroundColor: 'primary.dark',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-        >
-          {' '}
-          <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
-          <Typography variant={'body1'} sx={{ padding: '10%' }}>
-            Mark the tunes you know and the ones you would like to learn.
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            margin: '10px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            backgroundColor: 'primary.dark',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-        >
-          <PianoIcon color='secondary' sx={{ fontSize: 100 }} />
-          <Typography variant={'body1'} sx={{ padding: '10%' }}>
-            Find out which tunes you and your friends have in common.
-          </Typography>
-        </Box>
-      </Box>
+      )}
     </>
   );
 };
