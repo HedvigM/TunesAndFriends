97e16d81915b405651560c958bc13dc7f49a9c37
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-19 20:50:19 +0200

    feature: api detailed tune

--- utils/urls.tsx
+++ utils/urls.tsx
@@ -1,5 +1,4 @@
-/* export const POPULAR_URL =
-  'https://thesession.org/tunes/popular?format=json&page=1'; */
-
 const SESSION_URL = 'https://thesession.org/tunes/popular?format=json&page=';
 export const POPULAR_URL = (slug) => `${SESSION_URL}${slug}`;
+export const TUNE_URL = (slug) =>
+  `https://thesession.org/tunes/${slug}?format=json`;
