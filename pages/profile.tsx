8e22e75eda614bd03e459ef2a1c04a513dc0e3f7
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-29 19:22:23 +0200

    Create profile.tsx

---
+++ pages/profile.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import Container from '@mui/material/Container';
+import { Avatar, Box, Typography } from '@mui/material';
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
+import { useUser } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/router';
+
+export default function Profile() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  });
+  console.log('User', user);
+
+  return (
+    <>
+      <Header />
+      <Container
+        sx={{
+          borderRadius: 2,
+          boxShadow: 20,
+          fontWeight: 'fontWeightLight',
+          width: '75%',
+          paddingY: '10px',
+          marginY: '30px',
+        }}
+      >
+        <Typography textAlign='center' variant='h1'>
+          The Profile page
+        </Typography>
+        <Avatar
+          alt='users avatar'
+          sx={{ width: 50, height: 50 }}
+          src={user.picture}
+        />
+      </Container>
+
+      <Footer />
+    </>
+  );
+}
