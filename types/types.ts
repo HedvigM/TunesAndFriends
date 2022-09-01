2d572c117069fe925fef72374b47e1567ea008ff
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-09-01 15:26:32 +0200

    Create types.ts

---
+++ types/types.ts
@@ -0,0 +1,11 @@
+  export interface SuccessResponse<T> {
+    success: true;
+    data: T;
+  }
+  
+  export interface ErrorResponse {
+    success: false;
+    error: Error;
+  }
+  
+  export type ResponseType<T> = SuccessResponse<T> | ErrorResponse;
