f5116430b2d56446a2f442e620e65d4057e295af
HedvigM <hedvig@mejstedt.se>
2022-09-14 14:37:25 +0200

    feature: table to show friends

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 import Container from '@mui/material/Container';
-import { Typography } from '@mui/material';
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { useUser } from '@auth0/nextjs-auth0';
@@ -10,16 +17,17 @@ export default function Friends() {
   const { user } = useUser();
   const router = useRouter();
 
-  useEffect(() => {
+  /*  useEffect(() => {
     if (!user) {
       router.push('/');
     }
-  });
+  }); */
 
   return (
     <>
       <Header />
       <Container
+        maxWidth='sm'
         sx={{
           borderRadius: 2,
           boxShadow: 20,
@@ -32,6 +40,32 @@ export default function Friends() {
         <Typography textAlign='center' variant='h1'>
           The Friends page
         </Typography>
+        <Table
+          sx={{
+            maxWidth: '500px',
+            borderTop: '2px solid black',
+          }}
+        >
+          <TableBody>
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell>
+                <Typography textAlign='left' variant='body1'>
+                  Hej
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography textAlign='left' variant='body1'>
+                  lägg till/ ta bort vän
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Button>Gå till profil</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </Container>
 
       <Footer />
