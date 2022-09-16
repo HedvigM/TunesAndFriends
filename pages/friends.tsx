0ab9d0a7524087dbb5e9b3e297e9f6363686533d
HedvigM <hedvig@mejstedt.se>
2022-09-16 16:47:02 +0200

    feature: start on the friend/ profile page

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import {
   Button,
+  CircularProgress,
+  Link,
   Table,
   TableBody,
   TableCell,
@@ -12,16 +14,58 @@ import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { useUser } from '@auth0/nextjs-auth0';
 import { useRouter } from 'next/router';
+import { listUsers } from 'services/local';
 
 export default function Friends() {
   const { user } = useUser();
   const router = useRouter();
+  const [usersList, setUsersList] = useState();
+  const [loading, setLoading] = useState(false);
 
   /*  useEffect(() => {
     if (!user) {
       router.push('/');
     }
   }); */
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchListOfUsers = async () => {
+      const fetchedList = await listUsers();
+      if (fetchedList.success) {
+        setUsersList(fetchedList.data);
+      }
+      setLoading(false);
+    };
+
+    fetchListOfUsers();
+  }, []);
+  console.log('LIST:', usersList);
+
+  if (loading) {
+    return (
+      <>
+        <Header />
+
+        <Container
+          sx={{
+            borderRadius: 2,
+            boxShadow: 20,
+            fontWeight: 'fontWeightLight',
+            width: '75%',
+            paddingY: '10px',
+            marginY: '30px',
+            flexGrow: '1',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress color='primary' />
+        </Container>
+        <Footer />
+      </>
+    );
+  }
 
   return (
     <>
@@ -40,32 +84,43 @@ export default function Friends() {
         <Typography textAlign='center' variant='h1'>
           The Friends page
         </Typography>
-        <Table
-          sx={{
-            maxWidth: '500px',
-            borderTop: '2px solid black',
-          }}
-        >
-          <TableBody>
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        {usersList &&
+          usersList.map((user) => (
+            <Table
+              key={user.id}
+              sx={{
+                maxWidth: '500px',
+                borderTop: '2px solid black',
+              }}
             >
-              <TableCell>
-                <Typography textAlign='left' variant='body1'>
-                  Hej
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography textAlign='left' variant='body1'>
-                  lägg till/ ta bort vän
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Button>Gå till profil</Button>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+              <TableBody>
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell>
+                    <Typography textAlign='left' variant='body1'>
+                      {user.name}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography textAlign='left' variant='body1'>
+                      lägg till/ ta bort vän
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      href={{
+                        pathname: `/friend/[slug]`,
+                        query: { slug: `${user.id}` },
+                      }}
+                    >
+                      <a>Link to friend!</a>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          ))}
       </Container>
 
       <Footer />
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,24 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Button, Typography } from '@mui/material';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { useUser } from '@auth0/nextjs-auth0';
 import { useRouter } from 'next/router';
+import { getUser } from 'services/local';
+import { User } from '@prisma/client';
+/* Profile page and friend page is the same...  */
 
 export default function Profile() {
   const { user } = useUser();
   const router = useRouter();
+  const [databaseUser, setDatabaseUser] = useState<User>();
 
-  useEffect(() => {
+  /*   useEffect(() => {
     if (!user) {
       router.push('/');
     }
-  });
+  }); */
+  console.log('user', user);
+  console.log('databaseuser', databaseUser);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (user) {
+        const fetchedUser = await getUser(user.email);
+        if (fetchedUser.success) {
+          setDatabaseUser(fetchedUser.data);
+        }
+      }
+    };
+
+    fetchUser();
+  }, [user]);
 
   return (
     <>
       <Header />
+      {databaseUser && (
+        <Container
+          sx={{
+            borderRadius: 2,
+            boxShadow: 20,
+            fontWeight: 'fontWeightLight',
+            width: '75%',
+            paddingY: '10px',
+            marginY: '30px',
+          }}
+        >
+          <Box
+            sx={{ display: 'flex', justifyContent: 'center', padding: '30px' }}
+          >
+            <Avatar
+              alt='users avatar'
+              sx={{ width: 100, height: 100 }}
+              src={user.picture}
+            />
+          </Box>
+          <Typography
+            textAlign='center'
+            variant='h1'
+            sx={{ borderBottom: '2px solid black' }}
+          >
+            {databaseUser.name}, {databaseUser.town}
+          </Typography>
+          <Typography textAlign='center' variant='body1'>
+            {databaseUser.profileText}
+          </Typography>
+          <Button variant='contained'>Friends / become friends</Button>
+        </Container>
+      )}
       <Container
         sx={{
           borderRadius: 2,
@@ -29,18 +81,7 @@ export default function Profile() {
           marginY: '30px',
         }}
       >
-        <Typography textAlign='center' variant='h1'>
-          The Profile page
-        </Typography>
-        {user && (
-          <>
-            <Avatar
-              alt='users avatar'
-              sx={{ width: 50, height: 50 }}
-              src={user.picture}
-            />
-          </>
-        )}
+        <Typography variant='h2'>Tunes incommon:</Typography>
       </Container>
 
       <Footer />
