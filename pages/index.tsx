d16599c3e400dbf38bfc288a0181d932b23e8b09
HedvigM <hedvig@mejstedt.se>
2022-10-28 23:37:13 +0200

    fix: cleaning code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { Box, CircularProgress, Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import { useUser } from '@auth0/nextjs-auth0';
 import { User } from '@auth0/auth0-react';
 import { Header } from 'components/Header';
 import { TeasersList } from 'components/landingPage/TeasersList';
-import { ImageBG } from 'components/landingPage/ImageBG';
 import { Footer } from 'components/Footer';
 import { LoadingSpinner } from 'components/LoadingSpinner';
 
