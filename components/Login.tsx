db700ee64092c8650b5a9d7c220570aca493f655
Hedvig Mejstedt <hedvig@mejstedt.se>
2025-11-06 20:00:01 +0100

    100vw loginpage

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,6 +5,7 @@ export const Login = () => {
   return (
     <div style={{
       height: "100vh",
+      width: "100vw",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
